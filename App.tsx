808cf98f75f67e4dd5f2011ec3ade5ca27755893
Girish Godage <girishgodage@gmail.com>
2022-04-14 12:52:36 +0530

    ReactNative Biolerplate

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import Onboarding from './src/Authentication/Onboarding';
+import { LoadAssets } from './src/components';
+
+
+const fonts = {
+  "SFProDisplay-Bold": require("./assets/fonts/SF-Pro-Display-Bold.otf"),
+  "SFProDisplay-Semibold": require("./assets/fonts/SF-Pro-Display-Semibold.otf"),
+  "SFProDisplay-Medium": require("./assets/fonts/SF-Pro-Display-Medium.otf"),
+  "SFProDisplay-Regular": require("./assets/fonts/SF-Pro-Display-Regular.otf"),
+};
+
+const AuthenticationStack = createStackNavigator();
+
+
+const AuthenticationNavigator = () => {
+  return (    
+      <AuthenticationStack.Navigator headerMode="none">
+        <AuthenticationStack.Screen name="Onboarding" component={Onboarding} />
+      </AuthenticationStack.Navigator>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default function App() {
+  return (
+    <LoadAssets {...{ fonts }}>
+      <AuthenticationNavigator />
+    </LoadAssets>
+  );
+}
---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,22 @@
+import { View, Text ,StyleSheet } from 'react-native'
+import React from 'react'
+
+const Onboarding = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Onboarding</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'cyan',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+});
+
+export default Onboarding
